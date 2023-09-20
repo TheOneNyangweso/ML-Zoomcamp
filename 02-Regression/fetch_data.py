bbdde47ec5e2c1b0c1803c63ff33b15959c9d3d7
TheOneNyangweso <morurisammy5@gmail.com>
2023-09-20 19:04:26 +0300

    Updated fetch_data.py

--- 02-Regression/fetch_data.py
+++ 02-Regression/fetch_data.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 def fetch_save_csv(url, path):
     df = pd.read_csv(url)
-    df.to_csv(path ,index=False)
+    df.to_csv(path, index=False)
     return None
+
 
 url = 'https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-02-car-price/data.csv'
 path = '/home/nyangweso/Desktop/Ds_1/ML-Zoomcamp/02-Regression/car_price_data.csv'
